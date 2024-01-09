56ae379f6988cdd7483291d03d15678093e460f9
Ammar-Khaled <ammar.khaled.github@gmail.com>
2024-01-09 13:35:55 +0000

    Use older approach: the localeselector decorator

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -11,14 +11,15 @@ class Config():
     BABEL_DEFAULT_TIMEZONE = 'UTC'
 
 
+app = Flask(__name__)
+app.config.from_object('3-app.Config')
+babel = Babel(app)
+
+
+@babel.localeselector
 def get_locale() -> str:
     """get_locale method for localeselector."""
     return request.accept_languages.best_match(app.config['LANGUAGES'])
-
-
-app = Flask(__name__)
-app.config.from_object(Config())
-babel = Babel(app, locale_selector=get_locale)
 
 
 @app.route('/')
